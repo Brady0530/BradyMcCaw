3b7807b50227c1fd101cc59942f0056ef82c87a3
Brady <bgmccaw30@gmail.com>
2025-05-23 12:05:01 -0500

    Create BMinfo.py

---
+++ BMinfo.py
@@ -0,0 +1,75 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "BMImages/ramblinwreck.png"
+about_me = "My name is Brady McCaw. I am currently a 2nd year at Georgia Tech studying Industrial Engineering! "
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/brady-mccaw/"
+my_github_url = "https://github.com/Brady0530"
+my_email_address = "bgmccaw30@gmail.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Industrial Engineering',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': '2027',
+    'GPA': 'N/A'
+}
+course_data = {
+    "code":["CS 1301", "MATH 1554", "ECON 2100", "MGT 3000"], 
+    "names":["Intro to CS", "Linear Algebra", "Intro to Economics", "Financial and Managerial Accounting"], 
+    "semester_taken":["3rd", "2nd", "3rd", "2nd"],
+    "skills":["Python is not my favorite snake anymore", "I never want to see a matrix again", "All about money", "All about money but different"],
+    }
+experience_data = {
+    "Terrell Engineering Intern": (["- Worked on a barge for all of summer",
+                                                                    "- Got lots of oil and grease on my clothes", "- Lost my hearing to a fog horn"],"BMImages/bargepic.png"),
+    "Cook at Brooklyn Pizzeria":(["- Made the meanest pie you've ever seen",
+                                                           "- You don't want to know how we made the sauce"],"BMImages/pizzaguy.png"),
+    "Admissions intern":(["- Chief ambassador and admissions intern",
+                                                              "- In charge of getting new students"],"BMImages/recruitmentpic.jpg")
+
+}
+
+projects_data = {
+    "Online weather app made from scratch": "Used tips and tricks learned from CS 1301 to make an online weather webpage, one that uses just user input and one that incoprorates Gemini ai.",
+}
+
+programming_data = {
+    "Python": 80,
+    "Java": 60,
+    "C": 10,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+    "C": "🔍",
+}
+spoken_icons = {"French": "🇫🇷",
+    "English": "🇬🇧"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "French": "Fluent",
+}
+leadership_data = {
+    "Chair of GT Student Alumni Association First Year Program": (["- In charge of a bunch of freshmen"],"BMImages/gtsaa.png"),
+
+}
+activity_data={
+    "Spongebob fan club": ["- Spongebob role play every Thursday night", 
+            "- I'm Spongebob already you can't be Spongebob"]
+}
